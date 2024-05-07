64e00a0ba0aaf3a956c87bf6cf99f5920d2d6008
GhilbranMulya <220401023@student.umri.ac.id>
2024-05-07 20:39:38 +0700

    Second Commit

--- js/script.js
+++ js/script.js
@@ -14,72 +14,39 @@ document.addEventListener("click", function (e) {
 });
 
 // Slider Image
-(function () {
-  function Slideshow(element) {
-    this.el = document.querySelector(element);
-    this.init();
-  }
+const heroSlider = document.getElementById("hero-slider");
+const slides = Array.from(heroSlider.querySelectorAll("img.instagram-slide"));
 
-  Slideshow.prototype = {
-    init: function () {
-      this.wrapper = this.el.querySelector(".slider-wrapper");
-      this.slides = this.el.querySelectorAll(".slide");
-      this.index = 0;
-      this.total = this.slides.length;
-      this.timer = null;
+let index = 0;
+let interval;
 
-      this.action();
-      this.stopStart();
-    },
-
-    _slideTo: function (slide) {
-      var currentSlide = this.slides[slide];
-      currentSlide.style.opacity = 1;
-
-      for (var i = 0; i < this.slides.length; i++) {
-        var slide = this.slides[i];
-        if (slide !== currentSlide) {
-          slide.style.opacity = 0;
-        }
-      }
-    },
-
-    action: function () {
-      var self = this;
-      self.timer = setInterval(function () {
-        self.index++;
-        if (self.index == self.slides.length) {
-          self.index = 0;
-        }
-        self._slideTo(self.index);
-      }, 4000);
-    },
-
-    stopStart: function () {
-      var self = this;
-      self.el.addEventListener(
-        "mouseover",
-        function () {
-          clearInterval(self.timer);
-          self.timer = null;
-        },
-        false
-      );
-      self.el.addEventListener(
-        "mouseout",
-        function () {
-          self.action();
-        },
-        false
-      );
-    },
-  };
-
-  document.addEventListener("DOMContentLoaded", function () {
-    var slider = new Slideshow("#slider-utama");
+function showSlide() {
+  slides.forEach((slide, i) => {
+    slide.style.opacity = i === index ? 1 : 0;
+    slide.style.visibility = i === index ? "visible" : "hidden";
   });
-})();
-// End Slider image
+}
+
+function plusSlides(n) {
+  index += n;
+  if (index < 0) {
+    index = slides.length - 1;
+  } else if (index > slides.length - 1) {
+    index = 0;
+  }
+  showSlide();
+  resetInterval();
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(() => {
+    plusSlides(1);
+  }, 6000);
+}
+
+showSlide(); // Tampilkan slide pertama
+resetInterval();
 
 // Validation Form
 function validateForm() {
